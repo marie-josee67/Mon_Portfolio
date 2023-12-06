4e75fb1dd6ab2c5da4fe95e358f34ce65bea2edb
John Doe <mariejosee89@hotmail.fr>
2023-12-06 18:23:07 +0100

    Explication des différents string existante.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log('toto');
+/* test ==> console.log('toto');*/
+
+// string ==> chaine de caractère
+
+let myVar = "ma variable"; //  let ==> on peut changer sa valeur plus tard et la réasigner plus tard.
+
+const myVar2 ="ma second variable"; // const ==> bouge plus sa valeur.
+
+var myVar3 =" ma troisième variable"; // n'est plus utiliser de nos jours.
