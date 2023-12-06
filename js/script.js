14705d964cb0ef87a4f42c9f8643846e6b419607
John Doe <mariejosee89@hotmail.fr>
2023-12-06 18:36:13 +0100

    Cours suite de la découverte.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
 /* test ==> console.log('toto');*/
 
 // string ==> chaine de caractère
-
 let myVar = "ma variable"; //  let ==> on peut changer sa valeur plus tard et la réasigner plus tard.
+// si on veut la redéfinir
+myVar = " variable changée";
 
 const myVar2 ="ma second variable"; // const ==> bouge plus sa valeur.
 
 var myVar3 =" ma troisième variable"; // n'est plus utiliser de nos jours.
+
+console.log(myVar);
+
+//boolean
+let isTrue = true;
+let isFalse = false;
+
+console.log(isTrue);
+
+//chiffres et opérateurs
+
+let chiffre1 = 4; //pas de guillet sinon c'est pris pour un string
