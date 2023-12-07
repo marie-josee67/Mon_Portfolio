da99e0e04831620d314013c665b6432c053742d9
John Doe <mariejosee89@hotmail.fr>
2023-12-07 16:31:22 +0100

    Correction

--- js/script.js
+++ js/script.js
@@ -2,13 +2,20 @@
 function menuMobile(){
     const btn = document.querySelector(".burger");
     const header = document.querySelector(".header");
+    const links = document.querySelector(".navbar a"); /* cibler les liens */
 
     /* bouton click du burger */
     btn.addEventListener("click", ()  =>{
         /*console.log("click");*/
         /* ajoute au header cette classe  du coup lorsque l'on click sur le burger le menu apparait*/
         header.classList.toggle("show-nav"); /* toggle ==> ouvre et ferme le burger seulement sur les traits */
-    })
+    });
+
+    links.forEach(link => {
+        link.addEventListener("click", ()  =>{
+        header.classList.remove("show-nav"); 
+    });
+    });
 }
 
 /* jouer la function créer du click burger */
