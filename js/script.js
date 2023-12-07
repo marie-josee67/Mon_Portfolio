a9d84c514182974902c6e1344c4b4bcdd0101ef7
John Doe <mariejosee89@hotmail.fr>
2023-12-07 16:14:17 +0100

    Le burger s'ouvre et se ferme pour laisser apparaitre le menu.

--- js/script.js
+++ js/script.js
@@ -5,7 +5,9 @@ function menuMobile(){
 
     /* bouton click du burger */
     btn.addEventListener("click", ()  =>{
-        console.log("click");
+        /*console.log("click");*/
+        /* ajoute au header cette classe  du coup lorsque l'on click sur le burger le menu apparait*/
+        header.classList.toggle("show-nav"); /* toggle ==> ouvre et ferme le burger seulement sur les traits */
     })
 }
 
