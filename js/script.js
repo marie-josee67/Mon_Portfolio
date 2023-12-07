b699511ad1ff4b2b73f2186785b6a45fe75a0824
John Doe <mariejosee89@hotmail.fr>
2023-12-07 16:09:09 +0100

    Création de la function pour clicker sur le bouton burger.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 // function du thème
+function menuMobile(){
+    const btn = document.querySelector(".burger");
+    const header = document.querySelector(".header");
 
+    /* bouton click du burger */
+    btn.addEventListener("click", ()  =>{
+        console.log("click");
+    })
+}
 
+/* jouer la function créer du clicke burger */
+menuMobile();
