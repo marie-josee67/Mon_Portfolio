b155634bcaca594b2f01542f5db55ee5cafff841
John Doe <mariejosee89@hotmail.fr>
2023-12-07 18:10:52 +0100

    Filtre Js ne marche pas ici.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-// function du thème
+// ********************************************************* nav barre
 function menuMobile(){
     const btn = document.querySelector(".burger");
     const header = document.querySelector(".header");
-    const links = document.querySelector(".navbar a"); /* cibler les liens */
+    /*const links = document.querySelector(".navbar a"); /* cibler les liens */
+    const links = document.querySelectorAll(".navbar a"); /* Utiliser querySelectorAll pour sélectionner tous les liens */
 
     /* bouton click du burger */
     btn.addEventListener("click", ()  =>{
@@ -20,3 +21,31 @@ function menuMobile(){
 
 /* jouer la function créer du click burger */
 menuMobile();
+
+// ******************************************************* partie portfolio 
+
+// filtre des projets
+function tabsFilters (){
+    const tabs = document.querySelectorAll(".porfolio-filters a"); /* cible le portfolio */
+    const projets = document.querySelectorAll(".porfolio .card"); /* cible la carte */
+
+    /* liste toutes les cards */
+    const showProjets= () =>{
+        projets.forEach(projet =>{
+            console.log(projet);
+        });
+    }
+
+    tabs.forEach(elem =>{
+        elem.addEventListener("click", (event) => {
+            event.preventDefault(); /* ne suit pas l'action  il empêche sont fonctionnement natif de remonter en haut dans la page*/
+            let filter = elem.getAttribute("data-filter");
+            console.log(filter);
+        })
+    })
+
+    showProjets();
+}
+
+
+tabsFilters ()
